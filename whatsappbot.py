6a806198ffbd09de085756833050c7bf2b1214cf
higorsilveira <higorsilveira.hs@gmail.com>
2021-02-12 12:16:43 -0400

    Update whatsappbot.py

--- whatsappbot.py
+++ whatsappbot.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common import keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from random import randint
 import time
 import pygame
 
@@ -24,9 +25,9 @@ def main():
     time.sleep(3)
     global game_is_running
     while game_is_running:
-        print('entrou no loop')
-        texto = ler_mensagem("(//span[@class='_1VzZY selectable-text copyable-text']//span)[last()]")
-        print('digitado', texto)
+        #print('entrou no loop')
+        texto = ler_mensagem()
+        #print('digitado', texto)
         if texto[:3] == '/d':
             enviar_mensagem(mensagem)
         else:
@@ -44,6 +45,10 @@ def buscar_contato(contato):
     campo_pesquisa.send_keys(Keys.ENTER)
 
 def enviar_mensagem(mensagem):
+    
+    mensagem
+    
+
     campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
     campo_mensagem[1].click()
     time.sleep(1)
@@ -51,9 +56,16 @@ def enviar_mensagem(mensagem):
     time.sleep(1)
     campo_mensagem[1].send_keys(Keys.ENTER)
 
-def ler_mensagem(local):
-    leitura = driver.find_elements_by_xpath(local)
-    
-    return leitura
+def ler_mensagem():
+    leitura = driver.find_elements_by_xpath("(//span[@class='_1VzZY selectable-text copyable-text']//span)[last()]")
+    return leitura[0].text
+
+def rolar_dado(vezes, dado, plus):
+    result = 0 
+    for i in range(vezes):
+        result += randint(1,int(dado))
+    result += plus
+    return result
 
 main()
+
